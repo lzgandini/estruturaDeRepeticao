feb90a20dc50cc6274b35fce48c520c28e0d4732
Luiza Gandini <59964341+lzgandini@users.noreply.github.com>
2022-04-08 10:45:11 -0300

    Create #02.py

---
+++ #02.py
@@ -0,0 +1,12 @@
+"""Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário,
+mostrando uma mensagem de erro e voltando a pedir as informações."""
+
+nome = input("Informe seu nome de usuário: ")
+senha = input("Informe sua senha (diferente do nome): ")
+
+while nome == senha:
+    print("\nSeu nome de usuário e senha devem ser diferentes. Vamos de novo!\n")
+    nome = input("Informe seu nome de usuário: ")
+    senha = input("Informe sua senha (diferente do nome): ")
+
+print(f"\nObrigada {nome.capitalize()}!")
