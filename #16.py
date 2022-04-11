1d5f148989b0f27c4ac3f978927a8588dc5ac7ba
Luiza Gandini <59964341+lzgandini@users.noreply.github.com>
2022-04-11 12:53:53 -0300

    Create #16.py

---
+++ #16.py
@@ -0,0 +1,7 @@
+"""A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,...
+Faça um programa capaz de gerar a série até o n−ésimo termo."""
+
+fibonacci = (fibonacci - 1) + (fibonacci - 2)
+
+for c < 500:
+    print(fibonacci)
