9c57d2941c421fb6ad19dc33c7dfbddfbc3b6b2f
Luiza Gandini <59964341+lzgandini@users.noreply.github.com>
2022-04-14 13:04:49 -0300

    Update #32.py

--- #32.py
+++ #32.py
@@ -1,3 +1,6 @@
+"""O Departamento Estadual de Meteorologia lhe contratou para desenvolver um programa que leia as um conjunto indeterminado
+de temperaturas, e informe ao final a menor e a maior temperaturas informadas, bem como a média das temperaturas."""
+
 """Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
 Ex.: 5!=5.4.3.2.1=120. A saída deve ser conforme o exemplo abaixo:
 Fatorial de: 5
@@ -16,5 +19,6 @@ while i <= numero:
     numeroMultiplicador -= 1
     multiplicadores.append(numeroMultiplicador)
 
+print(f"\nFatorial de: {numero}")
 multiplicadoresStr = (str(multiplicadores)[1:-1]).replace(", ", " . ")
 print(f"{numero}! = {multiplicadoresStr} = {fatorial}")
